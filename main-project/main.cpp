62dca6412d3ba4751e6529914225dd7631ded933
azinhoooo <lisa555Kat@gmail.com>
2024-06-07 11:14:12 +0400

    Update main.cpp
    Добавлен вывод общей информации о задании

--- main-project/main.cpp
+++ main-project/main.cpp
@@ -4,8 +4,8 @@ using namespace std;
 
 int main()
 {
-    cout << "Laboratory work #8. GIT\n";
-    cout << "Variant #0. Library Subscription\n";
+    cout << "Laboratory work #4. GIT\n";
+    cout << "Variant #2. Conference program\n";
     cout << "Author: Sergey Radevich\n";
     return 0;
 }
