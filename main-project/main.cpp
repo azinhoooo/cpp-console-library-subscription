648f15b65a19e17d8af6ad974ea106b534411c49
azinhoooo <lisa555Kat@gmail.com>
2024-06-07 12:04:41 +0400

    Реализовано форматирование данных при выводе в консоль

--- main-project/main.cpp
+++ main-project/main.cpp
@@ -1,33 +1,56 @@
 #include <iostream>
+#include <iomanip>
 #include "file_reader.h"
 #include "conference_programm.h"
+#include "constants.h"
 using namespace std;
 
 int main()
 {
-    cout << "Laboratory work #4. GIT\n";
-    cout << "Variant #2. Conference program\n";
-    cout << "Author: Sergey Radevich\n";
-    conference_programm* subscriptions[MAX_FILE_ROWS_COUNT];
+    setlocale(LC_ALL, "Russian");
+    cout << "Лабораторная работа №4. GIT\n";
+    cout << "Вариант №2. Программа конференции\n";
+    cout << "Автор: Сергей Радевич\n\n";
+
+    conference_programm* programs[MAX_FILE_ROWS_COUNT];
     int size;
     try
     {
-        read("data.txt", subscriptions, size);
+        read("data.txt", programs, size);
+        cout << "***** Программа конференции *****\n\n";
         for (int i = 0; i < size; i++)
         {
-            cout << subscriptions[i]->start.hour << ' ';
-            cout << subscriptions[i]->start.minute << '\n';
-            cout << subscriptions[i]->finish.hour << ' ';
-            cout << subscriptions[i]->finish.minute << '\n';
-            cout << subscriptions[i]->author.last_name << '\n';
-            cout << subscriptions[i]->author.first_name << '\n';
-            cout << subscriptions[i]->author.middle_name << '\n';
-            cout << subscriptions[i]->title << '\n';
+            /********** вывод времени начала **********/
+            cout << "Время начала....: ";
+            cout << setw(2) << setfill('0') << programs[i]->start.hour << ':';
+            cout << setw(2) << setfill('0') << programs[i]->start.minute;
+            cout << '\n';
+
+            /********** вывод времени окончания **********/
+            cout << "Время окончания.: ";
+            cout << setw(2) << setfill('0') << programs[i]->finish.hour << ':';
+            cout << setw(2) << setfill('0') << programs[i]->finish.minute;
+            cout << '\n';
+
+            /********** вывод автора **********/
+            cout << "Автор...........: ";
+            // вывод фамилии
+            cout << programs[i]->author.last_name << " ";
+            // вывод первой буквы имени
+            cout << programs[i]->author.first_name[0] << ". ";
+            // вывод первой буквы отчества
+            cout << programs[i]->author.middle_name[0] << ".";
+            cout << '\n';
+
+            /********** вывод названия доклада **********/
+            cout << "Название........: ";
+            cout << '"' << programs[i]->title << '"';
+            cout << '\n';
             cout << '\n';
         }
         for (int i = 0; i < size; i++)
         {
-            delete subscriptions[i];
+            delete programs[i];
         }
     }
     catch (const char* error)
@@ -36,6 +59,5 @@ int main()
     }
     return 0;
 }
-
  
 
