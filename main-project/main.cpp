e6231efe23b54ed682b9a1387e36f257ecfd3233
azinhoooo <lisa555Kat@gmail.com>
2024-06-07 12:07:22 +0400

    Добавлены тестовые данные

--- main-project/main.cpp
+++ main-project/main.cpp
@@ -7,5 +7,6 @@ int main()
     cout << "Laboratory work #4. GIT\n";
     cout << "Variant #2. Conference program\n";
     cout << "Author: Sergey Radevich\n";
+    cout << "Group: 23PI1d\n";
     return 0;
 }
